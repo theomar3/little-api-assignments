a6e8d342ca55768e8b059a6c456778c2e7aa9963
Omar Ahmad <theomar3@gmail.com>
2016-08-08 14:19:58 -0500

    repo search finished

---
+++ day-21-github-repo-search/repo-search.js
@@ -0,0 +1,40 @@
+'use strict'
+
+if (this.JSApp === undefined) this.JSApp = {};
+
+(function(context) {
+
+  var $inputValue = $('#searchInput');
+  var $searchList = $('#searchList');
+
+  function searchRepo(data) {
+    console.log(data);
+
+    for (var i = 0; i < data.items.length; i++) {
+      var html =
+      '<li>'+data.items[i].full_name+'</li>';
+
+      $searchList.append(html);
+    }
+  }
+
+  function keyUpHappened(evt) {
+
+    if(evt.keyCode == 13) {
+      var promise = $.ajax({
+        url:'https://api.github.com/search/repositories?q='+$inputValue.val()
+      });
+
+      promise.done(searchRepo);
+    }
+  }
+
+  function start() {
+
+    $inputValue.on('keyup', keyUpHappened);
+
+  }
+
+
+  context.start = start;
+})(window.JSApp);
