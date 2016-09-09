d58ef92d67334a10321a4293c31b4a0dd1e9c191
Omar Ahmad <theomar3@gmail.com>
2016-09-09 14:33:03 -0500

    final update

--- day-20-cooking-steak-api/steak.js
+++ day-20-cooking-steak-api/steak.js
@@ -1,32 +1,84 @@
-$steakTable = $('#steak-table');
+'use strict'
+if (this.Steak === undefined) this.Steak = {};
+
+(function (context){
+  var $steakTable = $('#steak-table');
 
 
-var promise = $.ajax({
+  function steaks(data) {
+    $steakTable.empty();
+    console.log(data);
 
-  url:'http://tiy.ericsowell.com/api/cookingsteaks'
-});
+    var $templateHtml = $('#steak-template').html();
+    var htmlFactory = _.template($templateHtml);
 
-function recipeBook(data) {
-  console.log(data)
+    for(var i = 0; i < data.levels.length; i++) {
+      var html = htmlFactory(
+        {
+          type: data.levels[i].name,
+          description: data.levels[i].description,
+          picture: data.levels[i].imageUrl
+        }
+      );
+      $steakTable.append(html);
+    }
+  }
+
+  function start() {
+
+    $steakTable.html('Your table is loading, please wait');
+
+    var promise = $.ajax({
+      url:'http://tiy.ericsowell.com/api/cookingsteaks'
+    });
+
+    promise.done(steaks);
+
+  }
+  context.start = start;
+
+})(window.Steak);
 
 
-  for(i = 0; i < data.levels.length; i++) {
-    var html =
-      '<tr>' +
-      '<td>' +
-      data.levels[i].name +
-      '</td>' +
-      '<td>' +
-      data.levels[i].description +
-      '</td>' +
-      '<td>' +
-      '<img src="' + data.levels[i].imageUrl + '" />' +
-      '</td>' +
-      '</tr>'
-
-    $steakTable.append(html);
-  };
-}
 
 
-promise.done(recipeBook);
+
+
+
+
+
+
+
+
+// $steakTable = $('#steak-table');
+//
+//
+// var promise = $.ajax({
+//
+//   url:'http://tiy.ericsowell.com/api/cookingsteaks'
+// });
+//
+// function recipeBook(data) {
+//   console.log(data)
+//
+//
+//   for(i = 0; i < data.levels.length; i++) {
+//     var html =
+//       '<tr>' +
+//       '<td>' +
+//       data.levels[i].name +
+//       '</td>' +
+//       '<td>' +
+//       data.levels[i].description +
+//       '</td>' +
+//       '<td>' +
+//       '<img src="' + data.levels[i].imageUrl + '" />' +
+//       '</td>' +
+//       '</tr>'
+//
+//     $steakTable.append(html);
+//   };
+// }
+//
+//
+// promise.done(recipeBook);
